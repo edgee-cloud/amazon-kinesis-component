adba1a9101eeab718464def3ea4067d032d0a5bf
Alex Casalboni <alex@edgee.cloud>
2025-03-12 14:06:37 +0100

    update reference to .edgee/wit folder

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod kinesis_payload;
 
 wit_bindgen::generate!({
     world: "data-collection",
-    path: "wit",
+    path: ".edgee/wit",
     additional_derives: [serde::Serialize],
     generate_all,
 });
